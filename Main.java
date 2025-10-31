34bd06803b4de95b2ad41a4bb031fd3129e93507
Ph0enix-E <phoenixcedson@gmail.com>
2025-10-31 09:22:47 -0700

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+   private boolean dateChecked;
+   
+   public Main() {
+   dateChecked = false;
+   }
+  
+  
+   public boolean checkDate(int month1, int day1, int month2, int day2) {
+   boolean lesserDate = false;
+   if (month1 < month2) {
+      lesserDate = true;
+   }
+   else if (month1 == month2) {
+      if (day1 < day2) {
+         lesserDate = true;
+         }
+         else {
+         lesserDate = false;
+         }
+   }
+   else {
+      lesserDate = false;
+   }
+   return lesserDate;
+   }
+   
+   public static void main(String[] args) {
+      Main object1 = new Main();
+      boolean printYes = object1.checkDate(3, 20, 3, 20);
+      if (printYes == true) {
+      System.out.println("Date is before");
+      }
+      else {
+      System.out.println("Date isn't before");
+      }
+   }
+}
