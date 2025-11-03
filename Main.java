e14af7d29601651146b14b8f794d1d1cd4646dc0
Ph0enix-E <phoenixcedson@gmail.com>
2025-11-03 09:33:08 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
    }
   
   
-   public boolean checkDate(int month1, int day1, int month2, int day2) {
+   public boolean dateIsBefore(int month1, int day1, int month2, int day2) {
    boolean lesserDate = false;
    if (month1 < month2) {
       lesserDate = true;
@@ -27,10 +27,10 @@ public class Main {
    
    public static void main(String[] args) {
       Main object1 = new Main();
-      boolean printYes = object1.checkDate(6, 3, 9, 20);
-      boolean printYes2 = object1.checkDate(10, 1, 2, 25);
-      boolean printYes3 = object1.checkDate(8, 15, 8, 15);
-      boolean printYes4 = object1.checkDate(8, 15, 8, 16);
+      boolean printYes = object1.dateIsBefore(6, 3, 9, 20);
+      boolean printYes2 = object1.dateIsBefore(10, 1, 2, 25);
+      boolean printYes3 = object1.dateIsBefore(8, 15, 8, 15);
+      boolean printYes4 = object1.dateIsBefore(8, 15, 8, 16);
       System.out.println(printYes);
       System.out.println(printYes2);
       System.out.println(printYes3);
